295da813295f801ba14a2c5a96662832ee8688fe
eneax <19818937+eneax@users.noreply.github.com>
2019-04-18 21:23:00 +0200

    Add Poll to Store

--- src/actions/polls.js
+++ src/actions/polls.js
@@ -1,10 +1,36 @@
-// Action
-export const RECEIVE_POLLS = 'RECEIVE_POLLS'
+import { savePoll } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
-// Action Creator
+// Actions
+export const RECEIVE_POLLS = 'RECEIVE_POLLS'
+export const ADD_POLL = 'ADD_POLL'
+
+
+// Action Creators
 export function receivePolls(polls) {
   return {
     type: RECEIVE_POLLS,
     polls,
+  }
+}
+
+function addPoll (poll) {
+  return {
+    type: ADD_POLL,
+    poll,
+  }
+}
+
+export function handleAddPoll (poll) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+
+    dispatch(showLoading())
+    return savePoll({
+      ...poll,
+      author: authedUser
+    })
+      .then((poll) => dispatch(addPoll(poll)))
+      .then(() => dispatch(hideLoading()))
   }
 }
--- src/components/AddPoll.js
+++ src/components/AddPoll.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddPoll } from '../actions/polls'
 
 class AddPoll extends Component {
   state = {
@@ -30,7 +32,7 @@ class AddPoll extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     // Redirect to /homepage
-    console.log('Add poll: ', this.state)
+    this.props.dispatch(handleAddPoll(this.state))
   }
 
   render() {
@@ -97,4 +99,4 @@ class AddPoll extends Component {
   }
 }
 
-export default AddPoll 
+export default connect()(AddPoll) 
