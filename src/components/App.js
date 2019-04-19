6baa68770dba2f99f25af8ee31e5e11f46e2e1bf
eneax <19818937+eneax@users.noreply.github.com>
2019-04-19 23:46:40 +0200

    Poll state

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
 import Leaderboard from './Leaderboard'
 import AddPoll from './AddPoll'
+import Poll from './Poll'
 
 class App extends Component {
   componentDidMount() {
@@ -17,7 +18,7 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading === true
           ? null
-          : <AddPoll />
+          : <Poll match={{params: {id: 'loxhs1bqm25b708cmbf3g'}}}/>
         }
       </div>
     )
